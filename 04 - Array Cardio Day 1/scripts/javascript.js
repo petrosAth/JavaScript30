77614f53c952f240feb792ade43aedc9f063aa45
petrosAth <develop@athanasoulis.me>
2022-10-26 12:38:30 +0300

    Solve 04 - 6

--- 04 - Array Cardio Day 1/scripts/javascript.js
+++ 04 - Array Cardio Day 1/scripts/javascript.js
@@ -108,10 +108,15 @@ const oldest = inventors.sort(function(a, b) {
   let next = b.passed - b.year;
   return previous > next ? -1 : 1;
 });
-console.log(oldest);
+// console.log(oldest);
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+const category = document.querySelector(".mw-category");
+const links = Array.from(category.querySelectorAll("a"));
+const de = links
+  .map((link) => link.textContent)
+  .filter((streetName) => streetName.search("de") !== -1);
 
 // 7. sort Exercise
 // Sort the people alphabetically by last name
